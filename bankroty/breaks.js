b06fd3973d44684ead035832a58fc06c6f9d0677
Bára Soukupová <cvrczek@gmail.com>
2020-08-13 11:03:34 +0200

    přidány bankroty

---
+++ bankroty/breaks.js
@@ -0,0 +1,77 @@
+﻿var breaks = {
+  pob: {
+    title: "Podíl osob v bankrotu (%)",
+    step: 0.1,
+    decimals:1,
+    kraje: {
+      breaks: [0.8, 1.3, 1.8, 2.3],
+      colors: ['#eff3ff','#bdd7e7','#6baed6','#3182bd','#08519c']
+    },
+    okresy: {
+      breaks: [0.8, 1.3, 1.8, 2.3],
+      colors: ['#eff3ff','#bdd7e7','#6baed6','#3182bd','#08519c']
+    },
+    orp: {
+      breaks: [0.8, 1.3, 1.8, 2.3],
+      colors: ['#eff3ff','#bdd7e7','#6baed6','#3182bd','#08519c']
+    },
+    pou: {
+      breaks: [0.8, 1.3, 1.8, 2.3],
+      colors: ['#eff3ff','#bdd7e7','#6baed6','#3182bd','#08519c']
+    },
+    obce: {
+      breaks: [0.8, 1.3, 1.8, 2.3],
+      colors: ['#eff3ff','#bdd7e7','#6baed6','#3182bd','#08519c']
+    }
+  },
+  bve: {
+    title: "Osobní bankrot vs. exekuce (%)",
+    step: 0.1,
+    decimals:1,
+    kraje: {
+      breaks: [8, 12, 16, 20],
+      colors: ['#993404','#d95f0e','#fe9929','#fed98e','#ffffd4']
+    },
+    okresy: {
+      breaks: [8, 12, 16, 20],
+      colors: ['#993404','#d95f0e','#fe9929','#fed98e','#ffffd4']
+    },
+    orp: {
+      breaks: [8, 12, 16, 20],
+      colors: ['#993404','#d95f0e','#fe9929','#fed98e','#ffffd4']
+    },
+    pou: {
+      breaks: [8, 12, 16, 20],
+      colors: ['#993404','#d95f0e','#fe9929','#fed98e','#ffffd4']
+    },
+    obce: {
+      breaks: [8, 12, 16, 20],
+      colors: ['#993404','#d95f0e','#fe9929','#fed98e','#ffffd4']
+    }
+  },
+  pob_change9: {
+  title: "Procentuální změna počtu osob v bankrotu 2018 – 2019",
+  step: 0.1,
+  decimals:1,
+  kraje: {
+    breaks: [-8, -4,0, 4, 8],
+    colors: ['#b35806','#f1a340','#fee0b6','#d8daeb','#998ec3','#542788']
+  },
+  okresy: {
+    breaks: [-8, -4,0, 4, 8],
+    colors: ['#b35806','#f1a340','#fee0b6','#d8daeb','#998ec3','#542788']
+    },
+  orp: {
+    breaks: [-8, -4,0, 4, 8],
+    colors: ['#b35806','#f1a340','#fee0b6','#d8daeb','#998ec3','#542788']
+},
+pou: {
+  breaks: [-8, -4,0, 4, 8],
+  colors: ['#b35806','#f1a340','#fee0b6','#d8daeb','#998ec3','#542788']
+  },
+
+obce: {
+    breaks: [-8, -4,0, 4, 8],
+    colors: ['#b35806','#f1a340','#fee0b6','#d8daeb','#998ec3','#542788']
+    }
+}}
